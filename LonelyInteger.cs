e4d16149bc23e9d83b7d917456b164033ff44b32
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2022-11-11 19:57:47 +0200

    Create LonelyInteger.cs

---
+++ LonelyInteger.cs
@@ -0,0 +1,60 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'lonelyinteger' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int lonelyinteger(List<int> a)
+    {
+        Dictionary<int, int> elements = new Dictionary<int, int>();
+
+        foreach (var item in a)
+        {
+            if (!elements.ContainsKey(item))
+            {
+                elements.Add(item, 0);
+            }
+            elements[item]++;
+        }
+
+        return elements.OrderBy(x => x.Value).First().Key;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        int result = Result.lonelyinteger(a);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
