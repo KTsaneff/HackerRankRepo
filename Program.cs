6e9ee6ade41d431220d67b97d76f5900b55720df
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2023-07-12 08:15:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿int rows = int.Parse(Console.ReadLine());
+
+List<List<int>> arr = new List<List<int>>();
+
+for (int i = 0; i < rows; i++)
+{
+    List<int> list = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToList();
+
+    arr.Add(list);
+}
+
+int difference = DiagonalDifference(arr);
+Console.WriteLine(difference);
+
+static int DiagonalDifference(List<List<int>> arr)
+{
+    int sumFirstDiagonal = 0;
+    int sumSecondDiagonal = 0;
+
+    int firstCounter = 0;
+    int secondCounter = arr.Count -1;
+
+    for (int i = 0; i < arr.Count; i++)
+    {
+        sumFirstDiagonal += arr[i][firstCounter];
+        sumSecondDiagonal += arr[i][secondCounter];
+
+        firstCounter++;
+        secondCounter--;
+    }
+
+    return Math.Abs(sumFirstDiagonal - sumSecondDiagonal);
+}
